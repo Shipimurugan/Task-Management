4ff2d01616cbdfacf073476e21c7f9c6c02de7c4
Pari Balamurugan <shipibalamurugan2410@gmail.com>
2025-01-22 07:54:30 +0530

    Your commit message

--- task_management/authentication/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
-from authentication.models import *
-
-# Register your models here.
-
-class LoginUserGroup(ImportExportModelAdmin):
-    list_display=['id','user_name','password','is_admin','is_logged_in']
-admin.site.register(LoginUser,LoginUserGroup)
